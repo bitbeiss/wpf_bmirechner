ac4d4d62eb2e94be99c77de0a750a6927c68d772
Bitbrain <19reini91@gmail.com>
2018-10-15 13:39:06 +0200

    V2.1_15.10_18

---
+++ BmiCalculator/Calculator.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiCalculator
+{
+    public class Calculator : INotifyPropertyChanged, IDataErrorInfo
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool _Eingabefehler;
+        public bool Eingabefehler { get => _Eingabefehler; set => _Eingabefehler = value; }
+        private bool _Male;
+        public bool Male { get => _Male; set => _Male = value; }
+        private bool _Female;
+        public bool Female { get => _Female; set => _Female = value; }
+
+        private string _ClassificationResult;
+        public string ClassificationResult
+        {
+            get { return _ClassificationResult; }
+            set
+            {
+                _ClassificationResult = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("ClassificationResult"));
+            }
+        }
+
+        public void Classification()
+        {
+            if (Male)
+            {
+                if (Result < 20.0)
+                    ClassificationResult = "Untergewicht";
+                if ((Result > 20.0) && (Result < 25.0))
+                    ClassificationResult = "Normalgewicht";
+                if ((Result > 25.0) && (Result < 30.0))
+                    ClassificationResult = "Übergewicht";
+                if ((Result > 30) && (Result < 40))
+                    ClassificationResult = "Adipositas";
+                if (Result > 40)
+                    ClassificationResult = "massive Adipositas";
+            }
+            else if (Female)
+            {
+                if (Result < 19.0)
+                    ClassificationResult = "Untergewicht";
+                if ((Result > 19.0) && (Result < 24.0))
+                    ClassificationResult = "Normalgewicht";
+                if ((Result > 24.0) && (Result < 30.0))
+                    ClassificationResult = "Übergewicht";
+                if ((Result > 30) && (Result < 40))
+                    ClassificationResult = "Adipositas";
+                if (Result > 40)
+                    ClassificationResult = "massive Adipositas";
+            }
+            else
+                ClassificationResult = "Klassifikation";
+        }
+        public void Calculate()
+        {
+            if (Eingabefehler)
+            {
+                Result = 0;
+                return;
+            }
+            double dGrosse = Convert.ToDouble(Groesse) / 100.0;
+            double dGewicht = Convert.ToDouble(Gewicht);
+
+            Result = Math.Round(dGewicht / Math.Pow(dGrosse, 2.0), 1);
+            Classification();
+
+
+        }
+        public virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+        private double _Result;
+        public double Result
+        {
+            get { return _Result; }
+            set
+            {
+                _Result = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Result"));
+            }
+        }
+        private int _Groesse;
+        public int Groesse
+        {
+            get { return _Groesse; }
+            set
+            {
+                _Groesse = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Groesse"));
+            }
+        }
+
+        private int _Alter;
+        public int Alter
+        {
+            get { return _Alter; }
+            set
+            {
+                _Alter = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Alter"));
+            }
+        }
+
+        private int _Gewicht;
+        public int Gewicht
+        {
+            get { return _Gewicht; }
+            set
+            {
+                _Gewicht = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Gewicht"));
+            }
+        }
+
+        public string Error
+        {
+            get { return null; }
+        }
+
+        public string this[string propertyName]
+        {
+            get
+            {
+                if (propertyName == "Alter")
+                {
+                    if ((_Alter < 1) || (_Alter > 120))
+                        return "Das Alter muss zwischen 1 und 120 Jahren liegen.";
+                }
+                if (propertyName == "Gewicht")
+                {
+                    if ((_Gewicht < 1) || (_Gewicht > 300))
+                        return "Das Gewicht muss zwischen 1 und 300 Kilogramm liegen.";
+                }
+
+                if (propertyName == "Groesse")
+                {
+                    if ((_Groesse < 1) || (_Groesse > 350))
+                        return "Die Größe muss zwischen 1 und 350 cm sein.";
+                }
+                return null;
+            }
+        }
+    }
+}
--- BmiCalculator/MainWindow.xaml.cs
+++ BmiCalculator/MainWindow.xaml.cs
@@ -12,93 +12,38 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Diagnostics;
-using System.ComponentModel;
 
 namespace BmiCalculator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    /// 
-
     public partial class MainWindow : Window
     {
-
-        bool gewicht_ok;
-        bool alter_ok;
-        bool groesse_ok;
-
-        int Gewicht;
-        int Groesse;
-        int Alter;
-        int Bmi;
-        string Geschlecht;
-
+        Calculator BMIRechner = new Calculator { };
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = BMIRechner;
             _genderFemale.Style = FindResource("gender_selection_missing") as Style;
             _genderMale.Style = FindResource("gender_selection_missing") as Style;
-            
         }
 
-        private void OnTextChangedGewicht(object sender, TextChangedEventArgs e)
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
         {
-            int value;
-            if (int.TryParse(_gewicht.Text, out value))
-            {
-                if ((value > 0) && (value < 300))
-                {
-                    Gewicht = value;
-                    gewicht_ok = true;
-                }
-            }
-            else
-                _gewicht.BorderBrush = System.Windows.Media.Brushes.Red;
-                gewicht_ok = false;
-
-            Calculate();
-        }
-
-        private void OnTextChangedGroesse(object sender, TextChangedEventArgs e)
-        {
-            int value;
-            if (int.TryParse(_groesse.Text,out value))
-            {
-                if ((value > 0) && (value < 300))
-                    Groesse = value;
-            }
-            
-            Calculate();
-        }
-
-        private void OnTextChangedAlter(object sender, TextChangedEventArgs e)
-        {
-            int value;
-            if (int.TryParse(_alter.Text,out value))
-            {
-                if ((value > 0) && (value < 150))
-                    Alter = value;
-            }
-            
-            Calculate();
-        }
-
-        private void Calculate()
-        {
-            var alter = _alter.Text;
-
-            _result.Value = _result.Value + 10;
+            BMIRechner.Calculate();
         }
 
         private void CheckedChanged(object sender, RoutedEventArgs e)
         {
             _genderFemale.Style = FindResource("gender_selection_done") as Style;
             _genderMale.Style = FindResource("gender_selection_done") as Style;
-            Calculate();
+            BMIRechner.Female = Convert.ToBoolean(_genderFemale.IsChecked);
+            BMIRechner.Male = Convert.ToBoolean(_genderMale.IsChecked);
+
+
+            BMIRechner.Calculate();
         }
 
-        //Hallo
     }
 }
